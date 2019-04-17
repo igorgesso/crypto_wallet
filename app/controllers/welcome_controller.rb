af257e0ff3852b009689a2f20799ad1075bdc414
Igor Rezende <igorgesso@gmail.com>
2019-04-17 12:23:23 -0300

    Commit de Quarta-Feira Almoço - Aula 83

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    #cookies[:curso] = "Curso de Ruby on Rails - Igor Rezende"
     @meu_nome = params[:nome]
+    @curso = params[:curso]
   end
 end
