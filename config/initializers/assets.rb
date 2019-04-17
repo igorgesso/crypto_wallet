8ce4b6919f96b21e2c1e600bfdd44cdb33f39b2c
Igor Rezende <igorgesso@gmail.com>
2019-04-17 19:29:02 -0300

    Final Aula 91

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,15 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( cable.js
+                                                  coins.js
+                                                  welcome.js 
+                                                  mining_type.js
+                                                )
+                                                
+Rails.application.config.assets.precompile += %w( coins.css
+                                                  welcome.css 
+                                                  mining_type.css
+                                                  scaffolds.css    
+                                                )
+
